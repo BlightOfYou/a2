1b13e6a1dae2a0ebf3114dd5bfece47335120919
Christine Parry <blightofyou@gmail.com>
2014-03-16 16:46:22 -0400

    Login Frame now logs in

--- src/Phase2/src/edu/cmu/a2/ui/LoginFrame.java
+++ src/Phase2/src/edu/cmu/a2/ui/LoginFrame.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class LoginFrame extends javax.swing.JFrame {
 
-    private static final String databaseUrl = "jdbc:mysql://localhost:3306/inventory";
+    private static final String databaseUrl = "jdbc:mysql://localhost:3306/loginservice";
     public LoginService loginService = new LoginService(databaseUrl);
 //    private static class LoginService;
     
