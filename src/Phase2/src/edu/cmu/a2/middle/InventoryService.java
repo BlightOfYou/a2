5ee7d50d673cd11785d1fe6112c9b6b41c984c85
SupadupaK <kmljr@msn.com>
2014-03-18 19:13:28 -0400

    Trying my hand at inventory service, no idea.
    No idea what I'm doing.

--- src/Phase2/src/edu/cmu/a2/middle/InventoryService.java
+++ src/Phase2/src/edu/cmu/a2/middle/InventoryService.java
@@ -7,18 +7,73 @@ package edu.cmu.a2.middle;
 
 import edu.cmu.a2.dto.Product;
 import java.lang.ProcessBuilder.Redirect.Type;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 /**
  *
  */
 public class InventoryService {
-
+    
+    private String databaseUrl;
+    
     public InventoryService(String DatabaseUrl) {
-
+        databaseUrl = DatabaseUrl;
     }
 
     public Product GetProduct(String Type, int Id) {
+        // Database parameters
+        Boolean connectError = false;       // Error flag
+        Connection DBConn = null;           // MySQL connection handle
+        String errString = null;            // String for displaying errors
+        String msgString = null;            // String for displaying non-error messages
+        ResultSet res = null;               // SQL query result set pointer
+        Statement s = null;                 // SQL statement pointer
+        String type = Type;
+        int id = Id;
+        
+        try {
+            DBConn = DriverManager.getConnection(sourceURL,"remote","remote_pass");
+            
+            Statem
+        } catch (Exception e) {
+            errString =  "\nProblem connecting to database:: " + e;
+            connectError = true;
+        } // end try-catch
+        
+        
+        // If we are connected, then we get the product from the
+        // inventory database
+        
+        if ( !connectError )
+        {
+            try
+            {
+                s = DBConn.createStatement();
+                res = s.executeQuery( "SELECT * FROM inventory WHERE product = "+ id +";" ); /*Was hoping this selected a product with id*/
+
+                /*Now I am thinking of just getting the product*/
+                res.getString(1)
+                
+                /*Copied the below from Orders NewJFrame, don't know what this means...*/        
+                while (res.next())
+                {
+                    
+                    msgString = res.getString(1) + " : " + res.getString(2) +
+                            " : $"+ res.getString(4) + " : " + res.getString(3)
+                            + " units in stock";
+
+                } // while
+                
+            } catch (Exception e) {
+
+                errString =  "\nProblem getting product from inventory:: " + e;
+
+            } // end try-catch
+        } // if connect check
         return null;
 
     }
--- src/Phase2/src/edu/cmu/a2/middle/OrderService.java
+++ src/Phase2/src/edu/cmu/a2/middle/OrderService.java
@@ -6,21 +6,130 @@
 package edu.cmu.a2.middle;
 
 import edu.cmu.a2.dto.Order;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Calendar;
 import java.util.List;
 
 /**
  *
  */
 public class OrderService {
-
+    
+    private String databaseUrl;
+    private String firstName;
+    private String lastName;
+    
     public OrderService(String DatabaseUrl) {
+        databaseUrl = DatabaseUrl;
     }
 
     public void SubmitOrder(Order order) {
+        /*MOSTLY COPIED THIS FROM ORDERAPP*/
+        //If there is not a connection error, then we form the SQL statement
+        //to submit the order to the orders table and then execute it.
+
+        if (!connectError )
+        {
+            Calendar rightNow = Calendar.getInstance();
+
+            int TheHour = rightNow.get(rightNow.HOUR_OF_DAY);
+            int TheMinute = rightNow.get(rightNow.MINUTE);
+            int TheSecond = rightNow.get(rightNow.SECOND);
+            int TheDay = rightNow.get(rightNow.DAY_OF_WEEK);
+            int TheMonth = rightNow.get(rightNow.MONTH);
+            int TheYear = rightNow.get(rightNow.YEAR);
+            orderTableName = "order" + String.valueOf(rightNow.getTimeInMillis());
+
+            String dateTimeStamp = TheMonth + "/" + TheDay + "/" + TheYear + " "
+                    + TheHour + ":" + TheMinute  + ":" + TheSecond;
+
+            // Get the order data
+            firstName = Order.getFirstName(); //A get interface needs to be made for this 
+            lastName = Order.getLastName(); 
+            phoneNumber = jTextField5.getText();
+            customerAddress = jTextArea4.getText();
+            sTotalCost = jTextField6.getText();
+            beginIndex = 0;
+            beginIndex = sTotalCost.indexOf("$",beginIndex)+1;
+            sTotalCost = sTotalCost.substring(beginIndex, sTotalCost.length());
+            fCost = Float.parseFloat(sTotalCost);
+                
+            try
+            {
+                s = DBConn.createStatement();
+
+                SQLstatement = ( "CREATE TABLE " + orderTableName +
+                            "(item_id int unsigned not null auto_increment primary key, " +
+                            "product_id varchar(20), description varchar(80), " +
+                            "item_price float(7,2) );");
+
+                executeUpdateVal = s.executeUpdate(SQLstatement);
+
+            } catch (Exception e) {
+
+                errString =  "\nProblem creating order table " + orderTableName +":: " + e;
+                jTextArea3.append(errString);
+                executeError = true;
+
+            } // try
+
+            if ( !executeError )
+            {
+                try
+                {
+                    SQLstatement = ( "INSERT INTO orders (order_date, " + "first_name, " +
+                        "last_name, address, phone, total_cost, shipped, " +
+                        "ordertable) VALUES ( '" + dateTimeStamp + "', " +
+                        "'" + firstName + "', " + "'" + lastName + "', " +
+                        "'" + customerAddress + "', " + "'" + phoneNumber + "', " +
+                        fCost + ", " + false + ", '" + orderTableName +"' );");
+
+                    executeUpdateVal = s.executeUpdate(SQLstatement);
+                    
+                } catch (Exception e1) {
+
+                    errString =  "\nProblem with inserting into table orders:: " + e1;
+                    jTextArea3.append(errString);
+                    executeError = true;
+
+                    try
+                    {
+                        SQLstatement = ( "DROP TABLE " + orderTableName + ";" );
+                        executeUpdateVal = s.executeUpdate(SQLstatement);
+
+                    } catch (Exception e2) {
+
+                        errString =  "\nProblem deleting unused order table:: " +
+                                orderTableName + ":: " + e2;
+                        jTextArea3.append(errString);
+
+                    } // try
+
+                } // try
+
+            } //execute error check
 
     }
 
     public Order GetOrder(int OrderId) {
+        Connection DBConn = null;
+        try {
+            DBConn = DriverManager.getConnection(sourceURL,"remote","remote_pass");
+            
+            /*SQL command to select a single order from the SQL database*/
+            Statement s = DBConn.createStatement();
+            String SQLStatement = ("SELECT * FROM orders WHERE order_id = " + Integer.toString(OrderId) + "");
+            ResultSet res = s.executeQuery(SQLStatement);
+            
+            
+        }//try
+        catch (/**/){
+            
+        }
+        
         return null;
     }
 
